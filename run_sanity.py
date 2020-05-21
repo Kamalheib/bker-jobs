3415e4ffeed406d0af18db17d5170c56df321826
Kamal Heib <kheib@redhat.com>
2020-05-21 12:42:23 +0300

    bker-jobs: Add runner for sanity jobs
    Signed-off-by: Kamal Heib <kheib@redhat.com>

---
+++ run_sanity.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+
+job_list = [
+"bnxt-roce.xml",
+"cxgb4-iw.xml",
+"hfi1-opa.xml",
+"mlx4-ib.xml",
+"mlx4-roce.xml",
+"mlx5-ib.xml",
+"mlx5-roce.xml",
+"qedr-iw.xml"]
+
+
+whit_board = ""
+kernel_rpm = ""
+core_rpm = ""
+modules_rpm = ""
+
+for job in job_list:
+    os.system("sed -i 's,| RDMA sanity <,| RDMA sanity | %s<,g' %s" % (whit_board, job))
+    os.system("sed -i 's,URL_K=\"\",URL_K=\"%s\",g' %s" % (kernel_rpm, job))
+    os.system("sed -i 's,URL_C=\"\",URL_C=\"%s\",g' %s" % (core_rpm, job))
+    os.system("sed -i 's,URL_M=\"\",URL_M=\"%s\",g' %s" % (modules_rpm, job))
+    os.system("bkr job-submit %s" % (job))
