e3b9661640b85bb3bda8efe0e5c51b741a2015c4
Kamal Heib <kheib@redhat.com>
2020-10-12 18:34:15 +0300

    bker-jobs: Set the distro_name
    Signed-off-by: Kamal Heib <kheib@redhat.com>

--- run_sanity.py
+++ run_sanity.py
@@ -17,7 +17,7 @@ whit_board = ""
 kernel_rpm = ""
 core_rpm = ""
 modules_rpm = ""
-distro_name = ""
+distro_name = "RHEL-8.4.0-20201010.d.0"
 
 for job in job_list:
     os.system("sed -i 's,| RDMA sanity <,| RDMA sanity | %s<,g' %s" % (whit_board, job))
